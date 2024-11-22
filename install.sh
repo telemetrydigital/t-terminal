c9f1adf2f44c5f95c64badc29525266429e674c2
Pavol Krnáč <pavol.krnac@fabrika.digital>
2024-11-22 17:23:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Funkcia na zobrazenie logu
 log() {
   echo -e "[\033[1;32mINFO\033[0m] $1"
 }
 
-log "1/7: Aktualizujem systémové balíky..."
+log "1/8: Aktualizujem systémové balíky..."
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-log "2/7: Inštalujem potrebné balíky pre kiosk mode..."
+log "2/8: Inštalujem potrebné balíky pre kiosk mode..."
 sudo apt-get install -y python3 python3-pip python3-pyqt5 xorg xserver-xorg x11-xserver-utils xinit git matchbox-window-manager
 
-log "3/7: Klonujem repozitár tterminal..."
+log "3/8: Klonujem repozitár tterminal..."
 if [ ! -d "/opt/tterminal" ]; then
   sudo git clone https://github.com/telemetrydigital/tterminal.git /opt/tterminal
 else
@@ -19,21 +18,16 @@ else
   cd /opt/tterminal && sudo git pull
 fi
 
-log "4/7: Inštalujem Python závislosti..."
+log "4/8: Inštalujem Python závislosti..."
 sudo pip3 install -r /opt/tterminal/requirements.txt
 
-log "5/7: Nastavujem oprávnenia pre tty a video..."
-# Pridanie používateľa pi do potrebných skupín
+log "5/8: Nastavujem oprávnenia pre tty a video..."
 sudo usermod -aG tty pi
 sudo usermod -aG video pi
-
-# Nastavenie oprávnení pre /dev/tty0
 sudo chmod 660 /dev/tty0
 sudo chown root:tty /dev/tty0
 
-log "6/7: Nastavujem kiosk mode..."
-
-# Vytvorenie `.xinitrc` pre kiosk mode
+log "6/8: Nastavujem režim kiosk mode..."
 cat <<EOL > ~/.xinitrc
 #!/bin/bash
 xset -dpms        # Vypnutie šetriča obrazovky
@@ -44,7 +38,6 @@ matchbox-window-manager &  # Ľahký správca okien pre kiosk mode
 EOL
 chmod +x ~/.xinitrc
 
-# Vytvorenie služby pre kiosk mode
 sudo bash -c "cat > /etc/systemd/system/kiosk.service" << 'EOF'
 [Unit]
 Description=Kiosk Mode
@@ -54,19 +47,23 @@ After=multi-user.target
 Environment=DISPLAY=:0
 User=pi
 Group=tty
-ExecStart=/usr/bin/startx /usr/bin/python3 /opt/tterminal/main.py -- :0 vt1 -keeptty
+ExecStart=/usr/bin/startx
 Restart=always
 StandardInput=tty
 TTYPath=/dev/tty1
 
 [Install]
-WantedBy=graphical.target
+WantedBy=multi-user.target
 EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable kiosk
 
-log "7/7: Inštalácia dokončená! Po reštarte sa kiosk mode spustí automaticky."
+log "7/8: Vypínam štandardné desktopové prostredie..."
+sudo systemctl set-default multi-user.target
+sudo systemctl disable lightdm
+
+log "8/8: Inštalácia dokončená! Po reštarte sa kiosk mode spustí automaticky."
 read -p "Chcete reštartovať Raspberry Pi teraz? (y/n): " RESTART
 if [[ "$RESTART" == "y" || "$RESTART" == "Y" ]]; then
   sudo reboot
