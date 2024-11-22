002b46339258aea2d755941f88e07bbd9116294b
Pavol Krnáč <pavol.krnac@fabrika.digital>
2024-11-22 16:24:41 +0100

    Update display_settings.py

--- backend/display_settings.py
+++ backend/display_settings.py
@@ -1,1 +1,30 @@
+import os
+
+def set_resolution(resolution):
+    """
+    Nastavenie rozlíšenia obrazovky.
+    """
+    width, height = resolution.split("x")
+    os.system(f"xrandr --fb {width}x{height}")
+
+def set_rotation(rotation):
+    """
+    Nastavenie rotácie obrazovky.
+    """
+    rotation_map = {
+        "0°": "normal",
+        "90°": "right",
+        "180°": "inverted",
+        "270°": "left"
+    }
+    os.system(f"xrandr --output HDMI-1 --rotate {rotation_map[rotation]}")
+
+def toggle_cursor(hide):
+    """
+    Skrytie alebo zobrazenie kurzora.
+    """
+    if hide:
+        os.system("xsetroot -cursor empty_cursor empty_cursor")
+    else:
+        os.system("xsetroot -cursor_name left_ptr")
 
